d8e1b99175196f3b0470a576286895adca59eb9a
Ebinu-s <ebinusuneer2nd@gmail.com>
2020-05-23 09:33:46 +0530

    final commit

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 module TodoList
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,7 +21,7 @@ module TodoList
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    # config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
     
   end
 end
